00557602cb0b9cff22c0b9a5cbe21abf1ee07ea3
electricface <songwentai@deepin.com>
2018-03-31 10:57:12 +0800

    add com.deepin.api.cursorhelper

---
+++ com.deepin.api.cursorhelper/auto.go
@@ -0,0 +1,44 @@
+package cursorhelper
+
+import "errors"
+import "fmt"
+import "pkg.deepin.io/lib/dbus1"
+import "pkg.deepin.io/lib/dbusutil"
+import "pkg.deepin.io/lib/dbusutil/proxy"
+import "unsafe"
+
+/* prevent compile error */
+var _ = errors.New
+var _ dbusutil.SignalHandlerId
+var _ = fmt.Sprintf
+
+type CursorHelper struct {
+	cursorHelper // interface com.deepin.api.CursorHelper
+	proxy.Object
+}
+
+func NewCursorHelper(conn *dbus.Conn) *CursorHelper {
+	obj := new(CursorHelper)
+	obj.Object.Init_(conn, "com.deepin.api.CursorHelper", "/com/deepin/api/CursorHelper")
+	return obj
+}
+
+type cursorHelper struct{}
+
+func (v *cursorHelper) GetObject_() *proxy.Object {
+	return (*proxy.Object)(unsafe.Pointer(v))
+}
+
+func (*cursorHelper) GetInterfaceName_() string {
+	return "com.deepin.api.CursorHelper"
+}
+
+// method Set
+
+func (v *cursorHelper) GoSet(flags dbus.Flags, ch chan *dbus.Call, name string) *dbus.Call {
+	return v.GetObject_().Go_(v.GetInterfaceName_()+".Set", flags, ch, name)
+}
+
+func (v *cursorHelper) Set(flags dbus.Flags, name string) error {
+	return (<-v.GoSet(flags, make(chan *dbus.Call, 1), name).Done).Err
+}
