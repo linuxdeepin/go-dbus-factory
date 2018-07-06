c31f5411e0713aa3d68975a3ddd52e98cee8bc2a
electricface <songwentai@deepin.com>
2018-07-06 16:49:15 +0800

    update org.freedesktop.notifications
    Change-Id: I991ce9d196bfbac70f79644efa3226669139af21

--- org.freedesktop.notifications/auto.go
+++ org.freedesktop.notifications/auto.go
@@ -34,30 +34,46 @@ func (*notifications) GetInterfaceName_() string {
 	return "org.freedesktop.Notifications"
 }
 
-// method CloseNotification
+// method GetCapabilities
 
-func (v *notifications) GoCloseNotification(flags dbus.Flags, ch chan *dbus.Call, in0 uint32) *dbus.Call {
-	return v.GetObject_().Go_(v.GetInterfaceName_()+".CloseNotification", flags, ch, in0)
+func (v *notifications) GoGetCapabilities(flags dbus.Flags, ch chan *dbus.Call) *dbus.Call {
+	return v.GetObject_().Go_(v.GetInterfaceName_()+".GetCapabilities", flags, ch)
 }
 
-func (v *notifications) CloseNotification(flags dbus.Flags, in0 uint32) error {
-	return (<-v.GoCloseNotification(flags, make(chan *dbus.Call, 1), in0).Done).Err
-}
-
-// method GetCapbilities
-
-func (v *notifications) GoGetCapbilities(flags dbus.Flags, ch chan *dbus.Call) *dbus.Call {
-	return v.GetObject_().Go_(v.GetInterfaceName_()+".GetCapbilities", flags, ch)
-}
-
-func (*notifications) StoreGetCapbilities(call *dbus.Call) (arg0 []string, err error) {
-	err = call.Store(&arg0)
+func (*notifications) StoreGetCapabilities(call *dbus.Call) (capabilities []string, err error) {
+	err = call.Store(&capabilities)
 	return
 }
 
-func (v *notifications) GetCapbilities(flags dbus.Flags) (arg0 []string, err error) {
-	return v.StoreGetCapbilities(
-		<-v.GoGetCapbilities(flags, make(chan *dbus.Call, 1)).Done)
+func (v *notifications) GetCapabilities(flags dbus.Flags) (capabilities []string, err error) {
+	return v.StoreGetCapabilities(
+		<-v.GoGetCapabilities(flags, make(chan *dbus.Call, 1)).Done)
+}
+
+// method Notify
+
+func (v *notifications) GoNotify(flags dbus.Flags, ch chan *dbus.Call, app_name string, replaces_id uint32, app_icon string, summary string, body string, actions []string, hints map[string]dbus.Variant, expire_timeout int32) *dbus.Call {
+	return v.GetObject_().Go_(v.GetInterfaceName_()+".Notify", flags, ch, app_name, replaces_id, app_icon, summary, body, actions, hints, expire_timeout)
+}
+
+func (*notifications) StoreNotify(call *dbus.Call) (id uint32, err error) {
+	err = call.Store(&id)
+	return
+}
+
+func (v *notifications) Notify(flags dbus.Flags, app_name string, replaces_id uint32, app_icon string, summary string, body string, actions []string, hints map[string]dbus.Variant, expire_timeout int32) (id uint32, err error) {
+	return v.StoreNotify(
+		<-v.GoNotify(flags, make(chan *dbus.Call, 1), app_name, replaces_id, app_icon, summary, body, actions, hints, expire_timeout).Done)
+}
+
+// method CloseNotification
+
+func (v *notifications) GoCloseNotification(flags dbus.Flags, ch chan *dbus.Call, id uint32) *dbus.Call {
+	return v.GetObject_().Go_(v.GetInterfaceName_()+".CloseNotification", flags, ch, id)
+}
+
+func (v *notifications) CloseNotification(flags dbus.Flags, id uint32) error {
+	return (<-v.GoCloseNotification(flags, make(chan *dbus.Call, 1), id).Done).Err
 }
 
 // method GetServerInformation
@@ -66,35 +82,19 @@ func (v *notifications) GoGetServerInformation(flags dbus.Flags, ch chan *dbus.C
 	return v.GetObject_().Go_(v.GetInterfaceName_()+".GetServerInformation", flags, ch)
 }
 
-func (*notifications) StoreGetServerInformation(call *dbus.Call) (arg0 string, arg1 string, arg2 string, arg3 string, err error) {
-	err = call.Store(&arg0, &arg1, &arg2, &arg3)
+func (*notifications) StoreGetServerInformation(call *dbus.Call) (name string, vendor string, version string, spec_version string, err error) {
+	err = call.Store(&name, &vendor, &version, &spec_version)
 	return
 }
 
-func (v *notifications) GetServerInformation(flags dbus.Flags) (arg0 string, arg1 string, arg2 string, arg3 string, err error) {
+func (v *notifications) GetServerInformation(flags dbus.Flags) (name string, vendor string, version string, spec_version string, err error) {
 	return v.StoreGetServerInformation(
 		<-v.GoGetServerInformation(flags, make(chan *dbus.Call, 1)).Done)
 }
 
-// method Notify
-
-func (v *notifications) GoNotify(flags dbus.Flags, ch chan *dbus.Call, arg1 string, arg2 uint32, arg3 string, arg4 string, arg5 string, arg6 []string, arg7 map[string]dbus.Variant, arg8 int32) *dbus.Call {
-	return v.GetObject_().Go_(v.GetInterfaceName_()+".Notify", flags, ch, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8)
-}
-
-func (*notifications) StoreNotify(call *dbus.Call) (arg0 uint32, err error) {
-	err = call.Store(&arg0)
-	return
-}
-
-func (v *notifications) Notify(flags dbus.Flags, arg1 string, arg2 uint32, arg3 string, arg4 string, arg5 string, arg6 []string, arg7 map[string]dbus.Variant, arg8 int32) (arg0 uint32, err error) {
-	return v.StoreNotify(
-		<-v.GoNotify(flags, make(chan *dbus.Call, 1), arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8).Done)
-}
-
 // signal NotificationClosed
 
-func (v *notifications) ConnectNotificationClosed(cb func(in0 uint32, in1 uint32)) (dbusutil.SignalHandlerId, error) {
+func (v *notifications) ConnectNotificationClosed(cb func(id uint32, reason uint32)) (dbusutil.SignalHandlerId, error) {
 	if cb == nil {
 		return 0, errors.New("nil callback")
 	}
@@ -108,11 +108,38 @@ func (v *notifications) ConnectNotificationClosed(cb func(in0 uint32, in1 uint32
 		Name: v.GetInterfaceName_() + ".NotificationClosed",
 	}
 	handlerFunc := func(sig *dbus.Signal) {
-		var in0 uint32
-		var in1 uint32
-		err := dbus.Store(sig.Body, &in0, &in1)
+		var id uint32
+		var reason uint32
+		err := dbus.Store(sig.Body, &id, &reason)
 		if err == nil {
-			cb(in0, in1)
+			cb(id, reason)
+		}
+	}
+
+	return obj.ConnectSignal_(rule, sigRule, handlerFunc)
+}
+
+// signal ActionInvoked
+
+func (v *notifications) ConnectActionInvoked(cb func(id uint32, action_key string)) (dbusutil.SignalHandlerId, error) {
+	if cb == nil {
+		return 0, errors.New("nil callback")
+	}
+	obj := v.GetObject_()
+	rule := fmt.Sprintf(
+		"type='signal',interface='%s',member='%s',path='%s',sender='%s'",
+		v.GetInterfaceName_(), "ActionInvoked", obj.Path_(), obj.ServiceName_())
+
+	sigRule := &dbusutil.SignalRule{
+		Path: obj.Path_(),
+		Name: v.GetInterfaceName_() + ".ActionInvoked",
+	}
+	handlerFunc := func(sig *dbus.Signal) {
+		var id uint32
+		var action_key string
+		err := dbus.Store(sig.Body, &id, &action_key)
+		if err == nil {
+			cb(id, action_key)
 		}
 	}
 
