f132918ce13c2476d70a0ed410f8206944b1f331
electricface <songwentai@deepin.com>
2019-04-01 19:19:35 +0800

    change(api): update com.deepin.daemon.greeter

--- com.deepin.daemon.greeter/auto.go
+++ com.deepin.daemon.greeter/auto.go
@@ -50,6 +50,22 @@ func (v *greeter) GetScaleFactor(flags dbus.Flags) (factor float64, err error) {
 		<-v.GoGetScaleFactor(flags, make(chan *dbus.Call, 1)).Done)
 }
 
+// method GetScreenScaleFactors
+
+func (v *greeter) GoGetScreenScaleFactors(flags dbus.Flags, ch chan *dbus.Call) *dbus.Call {
+	return v.GetObject_().Go_(v.GetInterfaceName_()+".GetScreenScaleFactors", flags, ch)
+}
+
+func (*greeter) StoreGetScreenScaleFactors(call *dbus.Call) (factors map[string]float64, err error) {
+	err = call.Store(&factors)
+	return
+}
+
+func (v *greeter) GetScreenScaleFactors(flags dbus.Flags) (factors map[string]float64, err error) {
+	return v.StoreGetScreenScaleFactors(
+		<-v.GoGetScreenScaleFactors(flags, make(chan *dbus.Call, 1)).Done)
+}
+
 // method SetScaleFactor
 
 func (v *greeter) GoSetScaleFactor(flags dbus.Flags, ch chan *dbus.Call, factor float64) *dbus.Call {
@@ -58,4 +74,14 @@ func (v *greeter) GoSetScaleFactor(flags dbus.Flags, ch chan *dbus.Call, factor 
 
 func (v *greeter) SetScaleFactor(flags dbus.Flags, factor float64) error {
 	return (<-v.GoSetScaleFactor(flags, make(chan *dbus.Call, 1), factor).Done).Err
+}
+
+// method SetScreenScaleFactors
+
+func (v *greeter) GoSetScreenScaleFactors(flags dbus.Flags, ch chan *dbus.Call, factors map[string]float64) *dbus.Call {
+	return v.GetObject_().Go_(v.GetInterfaceName_()+".SetScreenScaleFactors", flags, ch, factors)
+}
+
+func (v *greeter) SetScreenScaleFactors(flags dbus.Flags, factors map[string]float64) error {
+	return (<-v.GoSetScreenScaleFactors(flags, make(chan *dbus.Call, 1), factors).Done).Err
 }
