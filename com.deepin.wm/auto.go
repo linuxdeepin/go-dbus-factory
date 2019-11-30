9c6171b19a31f14a780764ee6181a62943749c5d
electricface <songwentai@deepin.com>
2019-11-30 20:19:15 +0800

    change(api): update com.deepin.wm

--- com.deepin.wm/auto.go
+++ com.deepin.wm/auto.go
@@ -34,26 +34,6 @@ func (*wm) GetInterfaceName_() string {
 	return "com.deepin.wm"
 }
 
-// method ChangeCurrentWorkspaceBackground
-
-func (v *wm) GoChangeCurrentWorkspaceBackground(flags dbus.Flags, ch chan *dbus.Call, uri string) *dbus.Call {
-	return v.GetObject_().Go_(v.GetInterfaceName_()+".ChangeCurrentWorkspaceBackground", flags, ch, uri)
-}
-
-func (v *wm) ChangeCurrentWorkspaceBackground(flags dbus.Flags, uri string) error {
-	return (<-v.GoChangeCurrentWorkspaceBackground(flags, make(chan *dbus.Call, 1), uri).Done).Err
-}
-
-// method SwitchToWorkspace
-
-func (v *wm) GoSwitchToWorkspace(flags dbus.Flags, ch chan *dbus.Call, backward bool) *dbus.Call {
-	return v.GetObject_().Go_(v.GetInterfaceName_()+".SwitchToWorkspace", flags, ch, backward)
-}
-
-func (v *wm) SwitchToWorkspace(flags dbus.Flags, backward bool) error {
-	return (<-v.GoSwitchToWorkspace(flags, make(chan *dbus.Call, 1), backward).Done).Err
-}
-
 // method SwitchApplication
 
 func (v *wm) GoSwitchApplication(flags dbus.Flags, ch chan *dbus.Call, backward bool) *dbus.Call {
@@ -82,6 +62,56 @@ func (v *wm) GoBeginToMoveActiveWindow(flags dbus.Flags, ch chan *dbus.Call) *db
 
 func (v *wm) BeginToMoveActiveWindow(flags dbus.Flags) error {
 	return (<-v.GoBeginToMoveActiveWindow(flags, make(chan *dbus.Call, 1)).Done).Err
+}
+
+// method ToggleActiveWindowMaximize
+
+func (v *wm) GoToggleActiveWindowMaximize(flags dbus.Flags, ch chan *dbus.Call) *dbus.Call {
+	return v.GetObject_().Go_(v.GetInterfaceName_()+".ToggleActiveWindowMaximize", flags, ch)
+}
+
+func (v *wm) ToggleActiveWindowMaximize(flags dbus.Flags) error {
+	return (<-v.GoToggleActiveWindowMaximize(flags, make(chan *dbus.Call, 1)).Done).Err
+}
+
+// method MinimizeActiveWindow
+
+func (v *wm) GoMinimizeActiveWindow(flags dbus.Flags, ch chan *dbus.Call) *dbus.Call {
+	return v.GetObject_().Go_(v.GetInterfaceName_()+".MinimizeActiveWindow", flags, ch)
+}
+
+func (v *wm) MinimizeActiveWindow(flags dbus.Flags) error {
+	return (<-v.GoMinimizeActiveWindow(flags, make(chan *dbus.Call, 1)).Done).Err
+}
+
+// method ShowWorkspace
+
+func (v *wm) GoShowWorkspace(flags dbus.Flags, ch chan *dbus.Call) *dbus.Call {
+	return v.GetObject_().Go_(v.GetInterfaceName_()+".ShowWorkspace", flags, ch)
+}
+
+func (v *wm) ShowWorkspace(flags dbus.Flags) error {
+	return (<-v.GoShowWorkspace(flags, make(chan *dbus.Call, 1)).Done).Err
+}
+
+// method ShowWindow
+
+func (v *wm) GoShowWindow(flags dbus.Flags, ch chan *dbus.Call) *dbus.Call {
+	return v.GetObject_().Go_(v.GetInterfaceName_()+".ShowWindow", flags, ch)
+}
+
+func (v *wm) ShowWindow(flags dbus.Flags) error {
+	return (<-v.GoShowWindow(flags, make(chan *dbus.Call, 1)).Done).Err
+}
+
+// method ShowAllWindow
+
+func (v *wm) GoShowAllWindow(flags dbus.Flags, ch chan *dbus.Call) *dbus.Call {
+	return v.GetObject_().Go_(v.GetInterfaceName_()+".ShowAllWindow", flags, ch)
+}
+
+func (v *wm) ShowAllWindow(flags dbus.Flags) error {
+	return (<-v.GoShowAllWindow(flags, make(chan *dbus.Call, 1)).Done).Err
 }
 
 // method PerformAction
@@ -166,6 +196,16 @@ func (v *wm) SetWorkspaceBackground(flags dbus.Flags, index int32, uri string) e
 	return (<-v.GoSetWorkspaceBackground(flags, make(chan *dbus.Call, 1), index, uri).Done).Err
 }
 
+// method SetTransientBackground
+
+func (v *wm) GoSetTransientBackground(flags dbus.Flags, ch chan *dbus.Call, arg0 string) *dbus.Call {
+	return v.GetObject_().Go_(v.GetInterfaceName_()+".SetTransientBackground", flags, ch, arg0)
+}
+
+func (v *wm) SetTransientBackground(flags dbus.Flags, arg0 string) error {
+	return (<-v.GoSetTransientBackground(flags, make(chan *dbus.Call, 1), arg0).Done).Err
+}
+
 // method GetCurrentWorkspace
 
 func (v *wm) GoGetCurrentWorkspace(flags dbus.Flags, ch chan *dbus.Call) *dbus.Call {
@@ -180,6 +220,22 @@ func (*wm) StoreGetCurrentWorkspace(call *dbus.Call) (index int32, err error) {
 func (v *wm) GetCurrentWorkspace(flags dbus.Flags) (index int32, err error) {
 	return v.StoreGetCurrentWorkspace(
 		<-v.GoGetCurrentWorkspace(flags, make(chan *dbus.Call, 1)).Done)
+}
+
+// method WorkspaceCount
+
+func (v *wm) GoWorkspaceCount(flags dbus.Flags, ch chan *dbus.Call) *dbus.Call {
+	return v.GetObject_().Go_(v.GetInterfaceName_()+".WorkspaceCount", flags, ch)
+}
+
+func (*wm) StoreWorkspaceCount(call *dbus.Call) (count int32, err error) {
+	err = call.Store(&count)
+	return
+}
+
+func (v *wm) WorkspaceCount(flags dbus.Flags) (count int32, err error) {
+	return v.StoreWorkspaceCount(
+		<-v.GoWorkspaceCount(flags, make(chan *dbus.Call, 1)).Done)
 }
 
 // method SetCurrentWorkspace
@@ -244,6 +300,22 @@ func (v *wm) GetAccel(flags dbus.Flags, id string) (data []string, err error) {
 		<-v.GoGetAccel(flags, make(chan *dbus.Call, 1), id).Done)
 }
 
+// method GetDefaultAccel
+
+func (v *wm) GoGetDefaultAccel(flags dbus.Flags, ch chan *dbus.Call, id string) *dbus.Call {
+	return v.GetObject_().Go_(v.GetInterfaceName_()+".GetDefaultAccel", flags, ch, id)
+}
+
+func (*wm) StoreGetDefaultAccel(call *dbus.Call) (data []string, err error) {
+	err = call.Store(&data)
+	return
+}
+
+func (v *wm) GetDefaultAccel(flags dbus.Flags, id string) (data []string, err error) {
+	return v.StoreGetDefaultAccel(
+		<-v.GoGetDefaultAccel(flags, make(chan *dbus.Call, 1), id).Done)
+}
+
 // method SetAccel
 
 func (v *wm) GoSetAccel(flags dbus.Flags, ch chan *dbus.Call, data string) *dbus.Call {
@@ -270,6 +342,46 @@ func (v *wm) RemoveAccel(flags dbus.Flags, id string) error {
 	return (<-v.GoRemoveAccel(flags, make(chan *dbus.Call, 1), id).Done).Err
 }
 
+// method SetDecorationTheme
+
+func (v *wm) GoSetDecorationTheme(flags dbus.Flags, ch chan *dbus.Call, themeType string, themeName string) *dbus.Call {
+	return v.GetObject_().Go_(v.GetInterfaceName_()+".SetDecorationTheme", flags, ch, themeType, themeName)
+}
+
+func (v *wm) SetDecorationTheme(flags dbus.Flags, themeType string, themeName string) error {
+	return (<-v.GoSetDecorationTheme(flags, make(chan *dbus.Call, 1), themeType, themeName).Done).Err
+}
+
+// method SetDecorationDeepinTheme
+
+func (v *wm) GoSetDecorationDeepinTheme(flags dbus.Flags, ch chan *dbus.Call, deepinThemeName string) *dbus.Call {
+	return v.GetObject_().Go_(v.GetInterfaceName_()+".SetDecorationDeepinTheme", flags, ch, deepinThemeName)
+}
+
+func (v *wm) SetDecorationDeepinTheme(flags dbus.Flags, deepinThemeName string) error {
+	return (<-v.GoSetDecorationDeepinTheme(flags, make(chan *dbus.Call, 1), deepinThemeName).Done).Err
+}
+
+// method ChangeCurrentWorkspaceBackground
+
+func (v *wm) GoChangeCurrentWorkspaceBackground(flags dbus.Flags, ch chan *dbus.Call, uri string) *dbus.Call {
+	return v.GetObject_().Go_(v.GetInterfaceName_()+".ChangeCurrentWorkspaceBackground", flags, ch, uri)
+}
+
+func (v *wm) ChangeCurrentWorkspaceBackground(flags dbus.Flags, uri string) error {
+	return (<-v.GoChangeCurrentWorkspaceBackground(flags, make(chan *dbus.Call, 1), uri).Done).Err
+}
+
+// method SwitchToWorkspace
+
+func (v *wm) GoSwitchToWorkspace(flags dbus.Flags, ch chan *dbus.Call, backward bool) *dbus.Call {
+	return v.GetObject_().Go_(v.GetInterfaceName_()+".SwitchToWorkspace", flags, ch, backward)
+}
+
+func (v *wm) SwitchToWorkspace(flags dbus.Flags, backward bool) error {
+	return (<-v.GoSwitchToWorkspace(flags, make(chan *dbus.Call, 1), backward).Done).Err
+}
+
 // method PresentWindows
 
 func (v *wm) GoPresentWindows(flags dbus.Flags, ch chan *dbus.Call, xids []uint32) *dbus.Call {
@@ -282,22 +394,12 @@ func (v *wm) PresentWindows(flags dbus.Flags, xids []uint32) error {
 
 // method EnableZoneDetected
 
-func (v *wm) GoEnableZoneDetected(flags dbus.Flags, ch chan *dbus.Call, val bool) *dbus.Call {
-	return v.GetObject_().Go_(v.GetInterfaceName_()+".EnableZoneDetected", flags, ch, val)
+func (v *wm) GoEnableZoneDetected(flags dbus.Flags, ch chan *dbus.Call, enabled bool) *dbus.Call {
+	return v.GetObject_().Go_(v.GetInterfaceName_()+".EnableZoneDetected", flags, ch, enabled)
 }
 
-func (v *wm) EnableZoneDetected(flags dbus.Flags, val bool) error {
-	return (<-v.GoEnableZoneDetected(flags, make(chan *dbus.Call, 1), val).Done).Err
-}
-
-// method SetDecorationDeepinTheme
-
-func (v *wm) GoSetDecorationDeepinTheme(flags dbus.Flags, ch chan *dbus.Call, deepinThemeName string) *dbus.Call {
-	return v.GetObject_().Go_(v.GetInterfaceName_()+".SetDecorationDeepinTheme", flags, ch, deepinThemeName)
-}
-
-func (v *wm) SetDecorationDeepinTheme(flags dbus.Flags, deepinThemeName string) error {
-	return (<-v.GoSetDecorationDeepinTheme(flags, make(chan *dbus.Call, 1), deepinThemeName).Done).Err
+func (v *wm) EnableZoneDetected(flags dbus.Flags, enabled bool) error {
+	return (<-v.GoEnableZoneDetected(flags, make(chan *dbus.Call, 1), enabled).Done).Err
 }
 
 // signal WorkspaceBackgroundChanged
@@ -327,32 +429,6 @@ func (v *wm) ConnectWorkspaceBackgroundChanged(cb func(index int32, newUri strin
 	return obj.ConnectSignal_(rule, sigRule, handlerFunc)
 }
 
-// signal StartupReady
-
-func (v *wm) ConnectStartupReady(cb func(wm_name string)) (dbusutil.SignalHandlerId, error) {
-	if cb == nil {
-		return 0, errors.New("nil callback")
-	}
-	obj := v.GetObject_()
-	rule := fmt.Sprintf(
-		"type='signal',interface='%s',member='%s',path='%s',sender='%s'",
-		v.GetInterfaceName_(), "StartupReady", obj.Path_(), obj.ServiceName_())
-
-	sigRule := &dbusutil.SignalRule{
-		Path: obj.Path_(),
-		Name: v.GetInterfaceName_() + ".StartupReady",
-	}
-	handlerFunc := func(sig *dbus.Signal) {
-		var wm_name string
-		err := dbus.Store(sig.Body, &wm_name)
-		if err == nil {
-			cb(wm_name)
-		}
-	}
-
-	return obj.ConnectSignal_(rule, sigRule, handlerFunc)
-}
-
 // signal compositingEnabledChanged
 
 func (v *wm) ConnectCompositingEnabledChanged(cb func(enabled bool)) (dbusutil.SignalHandlerId, error) {
@@ -373,6 +449,85 @@ func (v *wm) ConnectCompositingEnabledChanged(cb func(enabled bool)) (dbusutil.S
 		err := dbus.Store(sig.Body, &enabled)
 		if err == nil {
 			cb(enabled)
+		}
+	}
+
+	return obj.ConnectSignal_(rule, sigRule, handlerFunc)
+}
+
+// signal wmCompositingEnabledChanged
+
+func (v *wm) ConnectWmCompositingEnabledChanged(cb func(enabled bool)) (dbusutil.SignalHandlerId, error) {
+	if cb == nil {
+		return 0, errors.New("nil callback")
+	}
+	obj := v.GetObject_()
+	rule := fmt.Sprintf(
+		"type='signal',interface='%s',member='%s',path='%s',sender='%s'",
+		v.GetInterfaceName_(), "wmCompositingEnabledChanged", obj.Path_(), obj.ServiceName_())
+
+	sigRule := &dbusutil.SignalRule{
+		Path: obj.Path_(),
+		Name: v.GetInterfaceName_() + ".wmCompositingEnabledChanged",
+	}
+	handlerFunc := func(sig *dbus.Signal) {
+		var enabled bool
+		err := dbus.Store(sig.Body, &enabled)
+		if err == nil {
+			cb(enabled)
+		}
+	}
+
+	return obj.ConnectSignal_(rule, sigRule, handlerFunc)
+}
+
+// signal workspaceCountChanged
+
+func (v *wm) ConnectWorkspaceCountChanged(cb func(count int32)) (dbusutil.SignalHandlerId, error) {
+	if cb == nil {
+		return 0, errors.New("nil callback")
+	}
+	obj := v.GetObject_()
+	rule := fmt.Sprintf(
+		"type='signal',interface='%s',member='%s',path='%s',sender='%s'",
+		v.GetInterfaceName_(), "workspaceCountChanged", obj.Path_(), obj.ServiceName_())
+
+	sigRule := &dbusutil.SignalRule{
+		Path: obj.Path_(),
+		Name: v.GetInterfaceName_() + ".workspaceCountChanged",
+	}
+	handlerFunc := func(sig *dbus.Signal) {
+		var count int32
+		err := dbus.Store(sig.Body, &count)
+		if err == nil {
+			cb(count)
+		}
+	}
+
+	return obj.ConnectSignal_(rule, sigRule, handlerFunc)
+}
+
+// signal WorkspaceSwitched
+
+func (v *wm) ConnectWorkspaceSwitched(cb func(from int32, to int32)) (dbusutil.SignalHandlerId, error) {
+	if cb == nil {
+		return 0, errors.New("nil callback")
+	}
+	obj := v.GetObject_()
+	rule := fmt.Sprintf(
+		"type='signal',interface='%s',member='%s',path='%s',sender='%s'",
+		v.GetInterfaceName_(), "WorkspaceSwitched", obj.Path_(), obj.ServiceName_())
+
+	sigRule := &dbusutil.SignalRule{
+		Path: obj.Path_(),
+		Name: v.GetInterfaceName_() + ".WorkspaceSwitched",
+	}
+	handlerFunc := func(sig *dbus.Signal) {
+		var from int32
+		var to int32
+		err := dbus.Store(sig.Body, &from, &to)
+		if err == nil {
+			cb(from, to)
 		}
 	}
 
