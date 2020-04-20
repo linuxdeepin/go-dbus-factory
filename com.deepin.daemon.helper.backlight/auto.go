5790d0e852da399e955e98d37083e69250d1d73c
quezhiyong <quezhiyong@uniontech.com>
2020-04-20 13:42:18 +0800

    change(api): update com.deepin.daemon.helper.Backlight
    add DDCCI

--- com.deepin.daemon.helper.backlight/auto.go
+++ com.deepin.daemon.helper.backlight/auto.go
@@ -43,3 +43,66 @@ func (v *backlight) GoSetBrightness(flags dbus.Flags, ch chan *dbus.Call, type0 
 func (v *backlight) SetBrightness(flags dbus.Flags, type0 uint8, name string, value int32) error {
 	return (<-v.GoSetBrightness(flags, make(chan *dbus.Call, 1), type0, name, value).Done).Err
 }
+
+type DDCCI struct {
+	ddcci // interface com.deepin.daemon.helper.Backlight.DDCCI
+	proxy.Object
+}
+
+func NewDDCCI(conn *dbus.Conn) *DDCCI {
+	obj := new(DDCCI)
+	obj.Object.Init_(conn, "com.deepin.daemon.helper.Backlight", "/com/deepin/daemon/helper/Backlight/DDCCI")
+	return obj
+}
+
+type ddcci struct{}
+
+func (v *ddcci) GetObject_() *proxy.Object {
+	return (*proxy.Object)(unsafe.Pointer(v))
+}
+
+func (*ddcci) GetInterfaceName_() string {
+	return "com.deepin.daemon.helper.Backlight.DDCCI"
+}
+
+// method CheckSupport
+
+func (v *ddcci) GoCheckSupport(flags dbus.Flags, ch chan *dbus.Call, edidChecksum string) *dbus.Call {
+	return v.GetObject_().Go_(v.GetInterfaceName_()+".CheckSupport", flags, ch, edidChecksum)
+}
+
+func (*ddcci) StoreCheckSupport(call *dbus.Call) (support bool, err error) {
+	err = call.Store(&support)
+	return
+}
+
+func (v *ddcci) CheckSupport(flags dbus.Flags, edidChecksum string) (support bool, err error) {
+	return v.StoreCheckSupport(
+		<-v.GoCheckSupport(flags, make(chan *dbus.Call, 1), edidChecksum).Done)
+}
+
+// method GetBrightness
+
+func (v *ddcci) GoGetBrightness(flags dbus.Flags, ch chan *dbus.Call, edidChecksum string) *dbus.Call {
+	return v.GetObject_().Go_(v.GetInterfaceName_()+".GetBrightness", flags, ch, edidChecksum)
+}
+
+func (*ddcci) StoreGetBrightness(call *dbus.Call) (value int32, err error) {
+	err = call.Store(&value)
+	return
+}
+
+func (v *ddcci) GetBrightness(flags dbus.Flags, edidChecksum string) (value int32, err error) {
+	return v.StoreGetBrightness(
+		<-v.GoGetBrightness(flags, make(chan *dbus.Call, 1), edidChecksum).Done)
+}
+
+// method SetBrightness
+
+func (v *ddcci) GoSetBrightness(flags dbus.Flags, ch chan *dbus.Call, edidChecksum string, value int32) *dbus.Call {
+	return v.GetObject_().Go_(v.GetInterfaceName_()+".SetBrightness", flags, ch, edidChecksum, value)
+}
+
+func (v *ddcci) SetBrightness(flags dbus.Flags, edidChecksum string, value int32) error {
+	return (<-v.GoSetBrightness(flags, make(chan *dbus.Call, 1), edidChecksum, value).Done).Err
+}
