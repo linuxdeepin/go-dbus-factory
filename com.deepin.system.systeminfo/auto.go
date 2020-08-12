80184acd4f1ec0826151446d3a87ca56ed084398
wubowen <wubowen@uniontech.com>
2020-08-12 16:07:29 +0800

    fix(systeminfo): use dmidecode to get cpu MHZ
    当获取不到cpu频率时,使用另一种方式再去获取
    
    Log: 获取CPU频率为空的时候启用备用方案
    Bug: https://pms.uniontech.com/zentao/bug-view-42396.html
    Change-Id: Ifa49cb937ce2819b8e7143ab8a887cc6b5bf9908
    Reviewed-on: http://gerrit.uniontech.com/c/go-dbus-factory/+/1685
    Tested-by: hudeng <hudeng@uniontech.com>
    Reviewed-by: hudeng <hudeng@uniontech.com>
    Reviewed-on: http://gerrit.uniontech.com/c/go-dbus-factory/+/2777
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: lichangze <lichangze@uniontech.com>
    Reviewed-by: songwentai <songwentai@uniontech.com>
    Tested-by: <mailman@uniontech.com>

---
+++ com.deepin.system.systeminfo/auto.go
@@ -0,0 +1,62 @@
+package systeminfo
+
+import "errors"
+import "fmt"
+import "pkg.deepin.io/lib/dbus1"
+import "pkg.deepin.io/lib/dbusutil"
+import "pkg.deepin.io/lib/dbusutil/proxy"
+import "unsafe"
+
+/* prevent compile error */
+var _ = errors.New
+var _ dbusutil.SignalHandlerId
+var _ = fmt.Sprintf
+var _ unsafe.Pointer
+
+type SystemInfo struct {
+	systemInfo // interface com.deepin.system.SystemInfo
+	proxy.Object
+}
+
+func NewSystemInfo(conn *dbus.Conn) *SystemInfo {
+	obj := new(SystemInfo)
+	obj.Object.Init_(conn, "com.deepin.system.SystemInfo", "/com/deepin/system/SystemInfo")
+	return obj
+}
+
+type systemInfo struct{}
+
+func (v *systemInfo) GetObject_() *proxy.Object {
+	return (*proxy.Object)(unsafe.Pointer(v))
+}
+
+func (*systemInfo) GetInterfaceName_() string {
+	return "com.deepin.system.SystemInfo"
+}
+
+// property MemorySizeHuman s
+
+func (v *systemInfo) MemorySizeHuman() proxy.PropString {
+	return proxy.PropString{
+		Impl: v,
+		Name: "MemorySizeHuman",
+	}
+}
+
+// property MemorySize t
+
+func (v *systemInfo) MemorySize() proxy.PropUint64 {
+	return proxy.PropUint64{
+		Impl: v,
+		Name: "MemorySize",
+	}
+}
+
+// property CurrentSpeed t
+
+func (v *systemInfo) CurrentSpeed() proxy.PropUint64 {
+	return proxy.PropUint64{
+		Impl: v,
+		Name: "CurrentSpeed",
+	}
+}
