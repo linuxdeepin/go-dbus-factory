58592a9303915f452c3349b26bfa7f111a374d0f
tujingwei <tujingwei@uniontech.com>
2021-05-20 14:30:49 +0800

    fix: 删除用户的指纹模板之后，在此之前开启的认证，还是有指纹的认证方式
    当删除用户所有的指纹模板之后，发送对应的信号出来刷新认证
    
    Log: 删除所有的指纹模板之后，刷新对应用户的认证请求
    Bug: https://pms.uniontech.com/zentao/bug-view-80608.html
    Change-Id: I4203416c44b8bd03eb49c462e5be9c9958dfacf6

--- com.deepin.daemon.authenticate/auto.go
+++ com.deepin.daemon.authenticate/auto.go
@@ -470,6 +470,32 @@ func (v *fingerprint) ConnectVerifyStatus(cb func(id string, code int32, msg str
 	return obj.ConnectSignal_(rule, sigRule, handlerFunc)
 }
 
+// signal FingerMouldClear
+
+func (v *fingerprint) ConnectFingerMouldClear(cb func(username string)) (dbusutil.SignalHandlerId, error) {
+	if cb == nil {
+		return 0, errors.New("nil callback")
+	}
+	obj := v.GetObject_()
+	rule := fmt.Sprintf(
+		"type='signal',interface='%s',member='%s',path='%s',sender='%s'",
+		v.GetInterfaceName_(), "FingerMouldClear", obj.Path_(), obj.ServiceName_())
+
+	sigRule := &dbusutil.SignalRule{
+		Path: obj.Path_(),
+		Name: v.GetInterfaceName_() + ".FingerMouldClear",
+	}
+	handlerFunc := func(sig *dbus.Signal) {
+		var username string
+		err := dbus.Store(sig.Body, &username)
+		if err == nil {
+			cb(username)
+		}
+	}
+
+	return obj.ConnectSignal_(rule, sigRule, handlerFunc)
+}
+
 // signal Touch
 
 func (v *fingerprint) ConnectTouch(cb func(id string, pressed bool)) (dbusutil.SignalHandlerId, error) {
