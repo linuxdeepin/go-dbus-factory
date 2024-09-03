9b78726cee9aeb0942832cc2bd1a9c0889b08013
fly602 <47801344+fly602@users.noreply.github.com>
2024-09-03 15:13:59 +0800

    chore: 后端接口整改 (#114)
    dde后端接口使用org.deepin.dde，去掉多余的接口
    
    Co-authored-by: fuleyi <fuleyi@uniontech.com>

--- session/com.deepin.daemon.kwayland/manual.go
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: 2018 - 2022 UnionTech Software Technology Co., Ltd.
-//
-// SPDX-License-Identifier: GPL-3.0-or-later
-
-package kwayland
-
-type Rect struct {
-	X, Y, Width, Height int32
-}
