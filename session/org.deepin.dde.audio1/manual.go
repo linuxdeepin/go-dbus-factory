005557e3a6fcefd8a4b57e64cc815374892c7d0d
fly602 <47801344+fly602@users.noreply.github.com>
2024-11-14 17:54:50 +0800

    fix(audio): 修复dbus端口数据结构不匹配问题 (#120)
    修复dbus端口数据结构不匹配问题
    
    Log: 解决音量热键不响应问题
    Bug: https://pms.uniontech.com/bug-view-281775.html
    Influence: 快捷键-音量相关

--- session/org.deepin.dde.audio1/manual.go
+++ session/org.deepin.dde.audio1/manual.go
@@ -7,6 +7,5 @@ package audio1
 type PortInfo struct {
 	Name        string
 	Description string
-	Priority    uint32
-	Available   int // 0: Unknown, 1: No, 2: Yes
+	Available   byte // 0: Unknown, 1: No, 2: Yes
 }
