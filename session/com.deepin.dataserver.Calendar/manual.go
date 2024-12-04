60b7b4b6dc49c4222c6203feaadea4d4c570fca0
fly602 <47801344+fly602@users.noreply.github.com>
2024-12-04 17:02:21 +0800

    fix(audio): 添加日历接口的调用 (#122)
    添加日历接口的调用
    
    Log: 添加日历接口的调用
    pms: BUG-290957

--- session/com.deepin.dataserver.Calendar/manual.go
+++ session/com.deepin.dataserver.Calendar/manual.go
@@ -1,10 +1,9 @@
-// SPDX-FileCopyrightText: 2018 - 2023 UnionTech Software Technology Co., Ltd.
+// SPDX-FileCopyrightText: 2018 - 2022 UnionTech Software Technology Co., Ltd.
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-package lunarcalendar1
+package Calendar
 
-// DayFestival type
 type DayFestival struct {
 	Year      int32
 	Month     int32
